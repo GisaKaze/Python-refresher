6f56c9d93e3fbe1e5eab99b62bcf35b243369d72
GisaKaze <fredson.coder@gmail.com>
2022-04-15 15:44:43 +0200

    Add variables

---
+++ 01_variables/code.py
@@ -0,0 +1,33 @@
+# -- Numbers and Floats --
+
+x = 15
+price = 9.99
+
+discount = 0.2
+
+result = price * (1 - discount)
+
+print(result)
+
+# -- Strings --
+
+name = "Fredson"
+
+print(name)
+print(name * 2)
+
+# -- Changing variables --
+# Variables are names for values.
+
+a = 25
+b = a
+
+# Here we've given the value '25' the names 'a' and 'b'.
+
+print(a)
+print(b)
+
+b = 17
+
+print(a)
+print(b)
