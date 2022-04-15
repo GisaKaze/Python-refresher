81c37a59555871fecae16bd5c08abdabeedd408c
GisaKaze <fredson.coder@gmail.com>
2022-04-15 15:45:02 +0200

    Add string formating

---
+++ 02_string_formatting/code.py
@@ -0,0 +1,22 @@
+name = 'Fredson'
+greeting = "Hello, Fredson"
+
+print(greeting)
+
+name = "Gisa"
+
+greeting = f'Hello, {name}'
+print(greeting)
+
+
+name = "Anne"
+print(
+    greeting
+)
+print(
+    f"Hello, {name}"
+)
+
+greeting = 'Hello, {} Today is {} your lucky {}'
+formatted = greeting.format("Fredson", "Friday", "day")
+print(formatted)
